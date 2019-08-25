7ef61efd7016746fde4fe9557e94611193fcc60f
Matthew McCord <mattm.eternal@gmail.com>
2019-08-24 23:29:06 -0700

    Update webpack config to copy over spritesheet

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 const isProduction = process.env.npm_lifecycle_event === 'build'
 
-module.exports = {
+let config = {
   entry: './src',
   devtool: !isProduction && 'source-map',
   module: {
@@ -33,10 +35,29 @@ module.exports = {
     new OptimizeCssAssetsPlugin({}),
     new MiniCssExtractPlugin({
       filename: '[name].css'
-    })
+    }),
+    new CopyPlugin([
+      { from: './src/res', to: './src/res' }
+    ]),
   ],
   devServer: {
     stats: 'minimal',
     overlay: true
   }
+};
+
+if(!isProduction) {
+  config.devtool = 'eval-source-map'
+} else {
+  config.plugins = config.plugins.concat([
+		new CompressionPlugin({
+			filename: "[path].gz[query]",
+			algorithm: "gzip",
+			test: /\.js$|\.html$/,
+			threshold: 0,
+			minRatio: 1
+		})
+  ])
 }
+
+module.exports = config;
